15c425d18bb12752a7f26e611b17f61f2803afb6
Nic Ledoux <NL15025100@gmail.com>
2022-06-22 17:49:07 -0700

    updated ports and .env variables

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const MongoClient = require('mongodb').MongoClient
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
 let db,
-    dbConnectionString = process.env.DB_STRING,
-    dbName = 'sample_mflix',
-    collection
+  dbConnectionString = process.env.DB_STRING,
+  dbName = "sample_mflix",
+  collection;
 
-MongoClient.connect(dbConnectionString)
-    .then(client => {
-        console.log(`Connected to Database`)
-        db = client.db(dbName)
-        collection = db.collection('movies')
-    })
+MongoClient.connect(dbConnectionString).then((client) => {
+  console.log(`Connected to Database`);
+  db = client.db(dbName);
+  collection = db.collection("movies");
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT || PORT}`);
+});
